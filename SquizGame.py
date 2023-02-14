016f8ea30c55c54bb06bdff9a8482026c12175c6
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-13 23:36:23 -0500

    added questions

--- SquizGame.py
+++ SquizGame.py
@@ -6,3 +6,10 @@ def display_score():
     pass
 def play_again():
     pass
+
+#Use a dictionary to hold the questions and answers
+questions = {
+    "What data set type is changeable, unordered, and a collection of key:value pairs" : "B",
+    "What data set type is a collection of ordered, unchangeable, and related data" : "B",
+    "What data set type is a collection of unordered, unindexed, and no duplicate values": "B"
+}
