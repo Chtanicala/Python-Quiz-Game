ab64d331c53e46e7be62181c83072d067507999f
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-14 00:01:10 -0500

    display score function

--- SquizGame.py
+++ SquizGame.py
@@ -8,21 +8,37 @@ def new_game():
         print(key)
         for i in options[question_num-1]:
             print(i)
-        guess = input(" Enter (A, B, C, or D").upper()
+        guess = input(" Enter (A, B, C, or D): ").upper()
         guesses.append(guess)
 
         correct_guesses += check_answer(questions.get(key), guess)
         question_num += 1
     display_score(correct_guesses, guesses)
+
 def check_answer(answer, guess):
     if answer == guess:
         print("Correct!")
         return 1
     else:
-        pritn ("Wrong!")
+        print ("Wrong!")
         return 0
-def display_score():
-    pass
+def display_score(correct_guesses, guesses ):
+    print("Results")
+    
+    print("Answers: ", end=" ")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end=" ")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+
+    print(str(score) + "%")
+
 def play_again():
     pass
 
