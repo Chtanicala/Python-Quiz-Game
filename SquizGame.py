728f80e671a596592dc8996bd2d594f2779e1674
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-13 23:42:01 -0500

    added question options

--- SquizGame.py
+++ SquizGame.py
@@ -9,7 +9,11 @@ def play_again():
 
 #Use a dictionary to hold the questions and answers
 questions = {
-    "What data set type is changeable, unordered, and a collection of key:value pairs" : "B",
+    "What data set type is changeable, unordered, and a collection of key:value pairs" : "A",
     "What data set type is a collection of ordered, unchangeable, and related data" : "B",
-    "What data set type is a collection of unordered, unindexed, and no duplicate values": "B"
+    "What data set type is a collection of unordered, unindexed, and no duplicate values": "C"
 }
+
+options =   [["A: Dictionary", "B: 2DList", "C: Tuple", "D: Set"],
+            ["A: Set", "B: Tuple", "C: 2DList", "D: Dictionary"],
+            ["A: 2DList", "B: Dictionary", "C: Set", "D: Tuple"]]
